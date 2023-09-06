72b94edce97882e5db4d887888dc02db33763f0b
marcleerink <marc.leerink@planet.com>
2023-09-06 21:58:20 +0200

    functionality for streaming in images from sentinel-hub, needs refactoring

---
+++ plastic_detection_service/evalscripts.py
@@ -0,0 +1,32 @@
+EVALSCRIPT_ALL_BANDS = """
+    //VERSION=3
+    function setup() {
+        return {
+            input: [{
+                bands: ["B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09",
+                "B10","B11","B12"],
+                units: "DN"
+            }],
+            output: {
+                bands: 13,
+                sampleType: "INT16"
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [sample.B01,
+                sample.B02,
+                sample.B03,
+                sample.B04,
+                sample.B05,
+                sample.B06,
+                sample.B07,
+                sample.B08,
+                sample.B8A,
+                sample.B09,
+                sample.B10,
+                sample.B11,
+                sample.B12];
+    }
+"""
