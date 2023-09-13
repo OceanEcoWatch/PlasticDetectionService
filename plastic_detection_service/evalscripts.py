c50215f6b64e0334370647641e1ed00a300b131e
marcleerink <marc.leerink@planet.com>
2023-09-13 18:26:05 +0200

    add SCL mask to evalscript, refactor raster_to_wgs to input resample_alg

--- plastic_detection_service/evalscripts.py
+++ plastic_detection_service/evalscripts.py
@@ -52,6 +52,36 @@ function evaluatePixel(sample) {
 }
 """
 
+L2A_12_BANDS_CLEAR_WATER_MASK = """
+//VERSION=3
+function setup() {
+  return {
+    input: [{
+      bands: ["B01", "B02", "B03", "B04", "B05", "B06", "B07",
+              "B08", "B8A", "B09", "B11", "B12", "SCL"],  // Include SCL
+      units: "DN"
+    }],
+    output: {
+      id: "default",
+      bands: 13,  // 12 original bands + 1 for water mask
+      sampleType: SampleType.UINT16
+    }
+  }
+}
+
+function evaluatePixel(sample) {
+  // Use SCL for clear water mask. SCL value 6 indicates clear water.
+  var waterMask = (sample.SCL !== 6) ? 1 : 0;
+
+  return [
+    sample.B01, sample.B02, sample.B03, sample.B04,
+    sample.B05, sample.B06, sample.B07, sample.B08,
+    sample.B8A, sample.B09, sample.B11, sample.B12,
+    waterMask  // Adding water mask as the 13th band
+  ];
+}
+"""
+
 NDVI_NDWI = """
     //VERSION=3
 
