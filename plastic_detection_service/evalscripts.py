e9a045181473f44036db9c758173dce5143ffd94
marcleerink <marc.leerink@planet.com>
2023-09-18 11:17:54 +0200

    predictor only read first 12 bands (not the clear water mask)

--- plastic_detection_service/evalscripts.py
+++ plastic_detection_service/evalscripts.py
@@ -21,37 +21,6 @@ function evaluatePixel(sample) {
 }
 """
 
-L2A_12_BANDS_CLOUD_MASK = """
-//VERSION=3
-function setup() {
-  return {
-    input: [{
-      bands: ["B01", "B02", "B03", "B04", "B05", "B06", "B07",
-            "B08", "B8A", "B09", "B11", "B12"],
-      units: "DN"
-    }],
-    output: {
-      id: "default",
-      bands: 13,  // 12 original bands + 1 for cloud mask
-      sampleType: SampleType.UINT16
-    }
-  }
-}
-
-function evaluatePixel(sample) {
-  // Your cloud mask logic here. 0 = no cloud, 1 = cloud.
-  // For demonstration, we'll set it to 0 (no cloud).
-  var cloudMask = 0;
-
-  return [
-    sample.B01, sample.B02, sample.B03, sample.B04,
-    sample.B05, sample.B06, sample.B07, sample.B08,
-    sample.B8A, sample.B09, sample.B11, sample.B12,
-    cloudMask  // Adding cloud mask as the 13th band
-  ];
-}
-"""
-
 L2A_12_BANDS_CLEAR_WATER_MASK = """
 //VERSION=3
 function setup() {
