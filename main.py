090409ac84c3f5aa94205bfeba50acc9afb46330
marcleerink <marc.leerink@planet.com>
2023-09-27 20:13:24 +0200

    add scaling to percentage and filtering of vector points below 30% probability

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ from plastic_detection_service.download_images import stream_in_images
 from plastic_detection_service.evalscripts import L2A_12_BANDS_CLEAR_WATER_MASK
 from plastic_detection_service.gdal_ds import get_gdal_ds_from_memory
 from plastic_detection_service.reproject_raster import raster_to_wgs84
+from plastic_detection_service.scaling import round_to_nearest_5_int, scale_pixel_values
 from plastic_detection_service.to_vector import (
     filter_out_no_data_polygons,
     polygonize_raster,
@@ -64,6 +65,10 @@ def main():
     for data in data_gen:
         for _d in data:
             if _d.content is not None:
+                timestamp = datetime.datetime.strptime(
+                    _d.headers["Date"], "%a, %d %b %Y %H:%M:%S %Z"
+                )
+                print(f"Processing image from {timestamp}...")
                 raster_ds = get_gdal_ds_from_memory(_d.content)
                 clear_water_mask = raster_to_wgs84(
                     raster_ds, target_bands=[13], resample_alg=gdal.GRA_NearestNeighbour
@@ -75,15 +80,16 @@ def main():
                 pred_raster = predictor.predict(
                     detector, data=io.BytesIO(_d.content), out_dir=out_dir
                 )
-                timestamp = datetime.datetime.strptime(
-                    _d.headers["Date"], "%a, %d %b %Y %H:%M:%S %Z"
-                )
-                pred_raster_ds = get_gdal_ds_from_memory(pred_raster)
+                scaled_pred_raster = scale_pixel_values(io.BytesIO(pred_raster))
+
+                pred_rounded = round_to_nearest_5_int(io.BytesIO(scaled_pred_raster))
+                pred_raster_ds = get_gdal_ds_from_memory(pred_rounded)
+
                 wgs84_raster = raster_to_wgs84(
                     pred_raster_ds, resample_alg=gdal.GRA_Cubic
                 )
                 pred_polys_ds = polygonize_raster(wgs84_raster)
-                pred_polys_ds = filter_out_no_data_polygons(pred_polys_ds)
+                pred_polys_ds = filter_out_no_data_polygons(pred_polys_ds, threshold=30)
 
                 bands = wgs84_raster.RasterCount
                 height = wgs84_raster.RasterYSize
@@ -116,6 +122,7 @@ def main():
                     print("Successfully added prediction raster to database.")
 
                     db_vectors = []
+                    print(pred_polys_ds.GetLayer().GetFeatureCount())
                     for feature in pred_polys_ds.GetLayer():
                         pixel_value = int(
                             json.loads(feature.ExportToJson())["properties"][
