e7796e4fdd533d202e5951865a83287c076bc904
marcleerink <marc.leerink@planet.com>
2024-03-13 16:27:02 +0100

    cleanup database module

--- plastic_detection_service/database/connect.py
+++ plastic_detection_service/database/connect.py
@@ -1,41 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from plastic_detection_service.config import DATABASE_URL
-from plastic_detection_service.database.models import (
-    Image,
-    PredictionVector,
-    SceneClassificationVector,
-    get_db_engine,
-)
-from plastic_detection_service.models import DownloadResponse, Raster, Vector
-
-
-class Insert:
-    def __init__(self, session: Session, engine=get_db_engine()):
-        self.session = session
-        self.engine = engine
-
-    def insert_image(
-        self, raster: Raster, download_response: DownloadResponse, image_url: str
-    ):
-        with self.session as session:
-            image = Image.from_response_and_raster(download_response, raster, image_url)
-            session.add(image)
-            session.commit()
-
-    def insert_prediction(self, prediction: Vector, image_id: int, model_id: int):
-        with self.session as session:
-            pv = PredictionVector.from_vector(prediction, image_id, model_id)
-            session.add(pv)
-            session.commit()
-
-    def insert_scl(self, scl: Vector, image_id: int):
-        with self.session as session:
-            sv = SceneClassificationVector.from_vector(scl, image_id)
-            session.add(sv)
-            session.commit()
 
 
 class DatabaseError(Exception):
@@ -45,8 +12,8 @@ class DatabaseError(Exception):
 
 def create_db_session():
     engine = create_engine(DATABASE_URL)
-    Session = sessionmaker(bind=engine)
-    return Session()
+    session = sessionmaker(bind=engine)
+    return session()
 
 
 def _execute_query(session, query):
@@ -58,6 +25,28 @@ def safe_execute_query(session, query):
     try:
         result = _execute_query(session, query)
         return result
+    except SQLAlchemyError as e:
+        session.rollback()
+        error_message = f"Database error: {str(e)}"
+        raise DatabaseError(error_message)
+
+
+def safe_insert(session, orm_object):
+    try:
+        with session as s:
+            s.add(orm_object)
+            s.commit()
+    except SQLAlchemyError as e:
+        session.rollback()
+        error_message = f"Database error: {str(e)}"
+        raise DatabaseError(error_message)
+
+
+def safe_bulk_insert(session, orm_objects):
+    try:
+        with session as s:
+            s.bulk_save_objects(orm_objects)
+            s.commit()
     except SQLAlchemyError as e:
         session.rollback()
         error_message = f"Database error: {str(e)}"
