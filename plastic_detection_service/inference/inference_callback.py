0881f14e3143864d106acfb4c7b510e0302df03b
marcleerink <marc.leerink@planet.com>
2024-03-19 21:14:19 +0100

    move ML imports into callback so github actions doesnt need to install them

--- plastic_detection_service/inference/inference_callback.py
+++ plastic_detection_service/inference/inference_callback.py
@@ -1,7 +1,6 @@
-from .sagemaker_model.code.inference import input_fn, model_fn, output_fn, predict_fn
-
 
 def local_inference_callback(payload: bytes) -> bytes:
+    from .sagemaker_model.code.inference import input_fn, model_fn, output_fn, predict_fn
     model = model_fn(".")
     array = input_fn(payload, "application/octet-stream")
     pred_array = predict_fn(array, model)
