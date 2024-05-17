0558c1e7359315ec175e0e07a81c375ae73f67c6
marcleerink <marc.leerink@planet.com>
2024-05-17 17:23:50 +0200

    retry when empty response is returned

--- src/inference/inference_callback.py
+++ src/inference/inference_callback.py
@@ -1,11 +1,14 @@
 import base64
 import json
+import logging
 from abc import ABC, abstractmethod
 
 import runpod
 from runpod import Endpoint
 
 from src import config
+
+LOGGER = logging.getLogger(__name__)
 
 
 class BaseInferenceCallback(ABC):
@@ -28,5 +31,16 @@ class RunpodInferenceCallback(BaseInferenceCallback):
         endpoint = Endpoint(self.endpoint_url)
 
         run_response = endpoint.run_sync(request_input, timeout=120)
+
+        max_retries = 3
+        retries = 0
+        if not run_response:
+            LOGGER.info("Retrying inference")
+            retries += 1
+            return self.__call__(payload)
+        if retries > max_retries:
+            raise RuntimeError("Max retries exceeded. Inference failed.")
+
         pred_bytes = base64.b64decode(json.loads(run_response)["prediction"])  # type: ignore
+
         return pred_bytes
